96a6a2f57f3d6c87eec139e7cb00e1d7f9af9784
Muhammad Akif Hussain <makifhussainmn@gmail.com>
2024-12-17 23:00:57 +0500

    1 commit

---
+++ first.js
@@ -0,0 +1,55 @@
+function1 = () =>{
+    alert("My Name is Muhammad Akif Hussain")
+}
+function2 = () =>{
+    alert("My Mobile Number  is 923013200651")
+}
+function3 = () =>{
+    alert("My Whatsapp Number  is 923013200651")
+}
+
+function4 = () =>{
+    alert("exampleOfCancalCase")
+}
+function5 = () =>{
+let sum1num = 202
+let sum2num = 20265
+let totalnum = sum1num+sum2num
+
+document.getElementById("boxfrostat").innerHTML="202 + 20265";
+document.getElementById("boxOutput").innerHTML=totalnum;
+}
+function6 = () =>{
+let sum1num = 20265
+let sum2num = 2024
+let totalnum = sum1num-sum2num
+
+document.getElementById("boxfrostat").innerHTML=" 20265 -2024";
+document.getElementById("boxOutput").innerHTML=totalnum;
+}
+function7 = () =>{
+let sum1num = 5
+let sum2num = 2
+let totalnum = sum1num*sum2num
+
+document.getElementById("boxfrostat").innerHTML=" 5 * 2";
+document.getElementById("boxOutput").innerHTML=totalnum;
+}
+function8 = () =>{
+let sum1num = 100
+let sum2num = 2
+let totalnum = sum1num/sum2num
+
+document.getElementById("boxfrostat").innerHTML=" 100/2";
+document.getElementById("boxOutput").innerHTML=totalnum;
+}
+function9=()=>{
+    document.getElementById("boxfrostat").innerHTML="";
+
+}
+function10=()=>{
+    document.getElementById("boxOutput").innerHTML="";
+    
+    
+}
+
